b087f0bb430ea333c12b16d050a7ed1bdcdcb370
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-07-22 16:31:41 -0400

    wrap Router

--- src/index.js
+++ src/index.js
@@ -6,4 +6,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+
+    <Router>
+    <App />
+    </Router>,
+    
+document.getElementById('root'));
